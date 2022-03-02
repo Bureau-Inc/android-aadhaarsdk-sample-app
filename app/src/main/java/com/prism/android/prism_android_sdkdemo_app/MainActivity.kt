b6b2c85b88609214a64808475d5081fbdcc8b997
Dev_Kishan <devkishan@keyvalue.systems>
2022-03-02 13:15:11 +0530

    Made merchant id and User id configurable in client app

--- app/src/main/java/com/prism/android/prism_android_sdkdemo_app/MainActivity.kt
+++ app/src/main/java/com/prism/android/prism_android_sdkdemo_app/MainActivity.kt
@@ -5,8 +5,12 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.Group
 import com.prism.android.prismandroidnativesdk.PrismCallBack
 import com.prism.android.prismandroidnativesdk.PrismEntryPoint
 import com.prism.android.prismandroidnativesdk.PrismInstanceProvider
@@ -17,42 +21,66 @@ import com.prism.android.prismsampleapp.R
 class MainActivity : AppCompatActivity() {
     lateinit var prism:PrismEntryPoint
     private var alertDialog:AlertDialog.Builder?=null
+    private var merchantId = ""
+    private var userId = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         alertDialog = AlertDialog.Builder(this)
-        prism = PrismInstanceProvider.getInstance(this,this)
-        prism.initialize("auth0|61dfbb6dfd33420071be7021","user123_random-8283",
-            object : PrismCallBack {
-                override fun onKYCFinished(
-                    aadhaarData: ClientAadhaarData?,
-                    methodName: String?,
-                    isSuccess: Boolean?,errorType:String
-                ) {
-                    if(!errorType.isNullOrEmpty())
-                    {
-                        if(errorType== ENDPOINTS_DOWN)
-                          Log.w("Aadhaar Error","No endpoints Available")
-                        else if(errorType== INVOID_AUTH_ERROR)
-                            Log.w("Aadhaar Error","Not authorized")
-                        else if(errorType== DIGILOCKER_ERROR)
-                            Log.w("Aadhaar Error","Digilocker Site error")
-                        else if(errorType== UIDAI_ERROR)
-                            Log.w("Aadhaar Error","UIDAI Site Error")
-                        else if(errorType== INTERNET_ERROR)
-                            Log.w("Aadhaar Error","Intenet Error")
-                        else if(errorType== SDK_ERROR)
-                            Log.w("Aadhaar Error","SDK Error")
-                        else if(errorType== USER_CANCELLED)
-                            Log.w("Aadhaar Error","Cancelled by user")
-                    }
-                    if(isSuccess==true)
-                        Log.w("TAG",aadhaarData?.jsonString.toString())
-                    else
-                        showDialog(methodName)
-                }
+        prism = PrismInstanceProvider.getInstance(this, this)
+        findViewById<Button>(R.id.init_button).setOnClickListener {
+            merchantId = findViewById<EditText>(R.id.merchant_id_edit).text.toString().trim()
+            userId = findViewById<EditText>(R.id.user_id_edit).text.toString().trim()
+            if (merchantId.isNotEmpty() && userId.isNotEmpty()){
+                prism.initialize(
+                    merchantId,
+                    userId,
+                    object : PrismCallBack {
+                        override fun onKYCFinished(
+                            aadhaarData: ClientAadhaarData?,
+                            methodName: String?,
+                            isSuccess: Boolean?, errorType: String
+                        ) {
+                            if (!errorType.isNullOrEmpty()) {
+                                if (errorType == ENDPOINTS_DOWN)
+                                    Log.w("Aadhaar Error", "No endpoints Available")
+                                else if (errorType == INVOID_AUTH_ERROR)
+                                    Log.w("Aadhaar Error", "Not authorized")
+                                else if (errorType == DIGILOCKER_ERROR)
+                                    Log.w("Aadhaar Error", "Digilocker Site error")
+                                else if (errorType == UIDAI_ERROR)
+                                    Log.w("Aadhaar Error", "UIDAI Site Error")
+                                else if (errorType == INTERNET_ERROR)
+                                    Log.w("Aadhaar Error", "Intenet Error")
+                                else if (errorType == SDK_ERROR)
+                                    Log.w("Aadhaar Error", "SDK Error")
+                                else if (errorType == USER_CANCELLED)
+                                    Log.w("Aadhaar Error", "Cancelled by user")
+                            }
+                            if (isSuccess == true)
+                                Log.w("TAG", aadhaarData?.jsonString.toString())
+                            else
+                                showDialog(methodName)
+                        }
 
-            },"https://en3yadd32v3d97l.m.pipedream.net/","https://en3yadd32v3d97l.m.pipedream.net/",false)
+                    },
+                    "https://en3yadd32v3d97l.m.pipedream.net/",
+                    "https://en3yadd32v3d97l.m.pipedream.net/",
+                    false
+                )
+                findViewById<Group>(R.id.aadhaar_flow_group).visibility= View.VISIBLE
+            }
+            else
+            {
+                Toast.makeText(this,"Please add merchantId and User Id",Toast.LENGTH_LONG).show()
+                if(merchantId.isEmpty())
+                findViewById<EditText>(R.id.merchant_id_edit).error = "This field can't be blank"
+                if(userId.isEmpty())
+                findViewById<EditText>(R.id.user_id_edit).error = "This field can't be blank"
+            }
+        }
+
+
         findViewById<Button>(R.id.button1).setOnClickListener {
 
             prism.addConfig(Config(residentUidaiAadhaarFlow, myAadhaarUidaiFlow, digilockerFlow))
